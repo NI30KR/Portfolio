0ca745b5704fddf36790284f8b49fe4c12f147fd
Nitish Kumar <97013773+NI30KR@users.noreply.github.com>
2024-08-12 16:21:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  var typingEffect = new Typed(".typedText",{
-    strings : ["Designer","Youtuber","Developer"],
+    strings : ["Student","an Engineer","Front-End Developer"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
